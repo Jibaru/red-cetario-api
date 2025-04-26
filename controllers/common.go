c0571d717c8651d5ce4e1c5fc02b1e3127befefa
Jibaru <ignacioruedaboada@hotmail.com>
2025-04-25 20:01:52 -0500

    fix: add logs to check logger existence

--- controllers/common.go
+++ controllers/common.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"log"
 	"log/slog"
 
 	"github.com/gin-gonic/gin"
@@ -8,9 +9,13 @@ import (
 
 func GetLogger(c *gin.Context) *slog.Logger {
 	if v, exists := c.Get("logger"); exists {
+		log.Println("logger found in context")
+
 		if lg, ok := v.(*slog.Logger); ok {
+			log.Println("logger retrieved from context")
 			return lg
 		}
 	}
+	log.Println("logger not found in context, using default logger")
 	return slog.Default()
 }
